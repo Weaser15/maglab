df16441bda010b7499806797d5f91541afbbff90
Szymon Oramus <weaserpro@gmail.com>
2026-09-16 17:12:10 +0100

    Add MOKE docstring

--- src/maglab/formats/moke.py
+++ src/maglab/formats/moke.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def read_moke_scan(filepath: Path | str) -> pd.DataFrame:
+    """Reads a low-field MOKE table (currently up to v3.1) and outputs a DataFrame."""
     filepath = Path(filepath)
     df = pd.read_table(
         filepath,
