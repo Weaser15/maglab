67679d2b04529fbadf176221e27c20a11b0d07d9
Szymon Oramus <weaserpro@gmail.com>
2026-09-16 17:03:36 +0100

    Add MOKE format

---
+++ src/maglab/formats/moke.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pandas as pd
+
+
+def read_moke_scan(filepath: Path | str) -> pd.DataFrame:
+    filepath = Path(filepath)
+    df = pd.read_table(
+        filepath,
+        comment="#",
+        header=None,
+        sep=r"\s+",
+        names=["field", "mag"],
+        dtype={"field": "float32", "mag": "float32"},
+        engine="c",
+    )
+    return df
