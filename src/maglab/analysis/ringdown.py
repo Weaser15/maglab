f00c704f2f8f7926b22e1def404f6543ffb5708a
Szymon Oramus <weaserpro@gmail.com>
2026-09-17 09:47:17 +0100

    Add ringdown

--- src/maglab/analysis/ringdown.py
+++ src/maglab/analysis/ringdown.py
@@ -1,14 +1,18 @@
 from typing import cast
 
 import numpy as np
+import pandas as pd
 import scipy.fft as fft
 
 
-def averaged_magnetisation_ringdown(time: np.ndarray, mag: np.ndarray):
+def avg_mag_ringdown(time: np.ndarray, mag: np.ndarray):
 
     ntime = len(time)
     dt = np.ptp(time) / ntime
     frequencies = np.round(fft.fftfreq(ntime, dt) * 1e-9, 4)
+    mask = frequencies > 0
 
     m_fft = cast(np.ndarray, fft.rfft(mag))
-    psd = abs(m_fft) ** 2
+    psd = m_fft[mask] ** 2
+    freq = frequencies[mask]
+    return pd.DataFrame({"frequency": freq, "absorption": psd})
